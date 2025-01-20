149669aa46e1e95cc8007db893e368cc3393b4ff
sarwar-hossain <sarwar163264@gmail.com>
2025-01-20 22:55:36 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,24 @@ import React from "react";
 import './style/Navbar.css';
 
 
-function Navbar(){
-    return(
-        <>
+function Navbar() {
+  return (
+    <>
       <div className="navbar">
         <div className="logo">Shop App</div>
         <div className="page">
-            <ul>
-                <li>Home</li>
-                <li>Shop</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="shop">Shop</a></li>
+            <li><a href="about">About</a></li>
+            <li><a href="contact">Contact</a></li>
+          </ul>
         </div>
 
         <p className="login-btn">Login</p>
       </div>
-        </>
-    )
+    </>
+  )
 }
 
 export default Navbar
